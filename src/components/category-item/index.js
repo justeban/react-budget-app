3429be6987f9606bed25f6744ecc532bf8aec6fc
Justin Morris <justinjodymorris@gmail.com>
2018-08-10 11:14:42 -0700

    changed layout in order to modify css layout

--- src/components/category-item/index.js
+++ src/components/category-item/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 import CategoryForm from '../category-form/index.js';
-import ExpenseForm from '../expense-form/index.js';
-import ExpenseItem from '../expense-item/index.js';
 
-import {connect} from 'react-redux';
-import{expenseCreate, expenseDestroy, expenseUpdate} from '../../app/actions/expenses.js';
-
-class CategoryItem extends React.Component {
+export default class CategoryItem extends React.Component {
 
   constructor(props) {
     super(props);
@@ -26,6 +21,7 @@ class CategoryItem extends React.Component {
   }
 
   handleBudgetChange = () => {
+    console.log("HELLOW HERE", this.props.expenses);
     let expAmt = this.props.expenses[this.props.category.id].reduce((acc, exp) => {
       acc += parseFloat(exp.amountSpent);
       return acc;
@@ -49,36 +45,7 @@ class CategoryItem extends React.Component {
         {
           this.state.editing ? <CategoryForm handler={this.props.handleUpdate} category={this.props.category} toggle={this.toggleEditing}/> : null
         }
-        <h4>Add An Expense Item: </h4>
-        <ExpenseForm handler={this.props.handleCreateExpense} category={this.props.category}/>
-        <div className="expense-items">
-          {
-            this.props.expenses[this.props.category.id].map((expense, i) => 
-              <div key={i} className="expense-item">
-                <ExpenseItem 
-                  key={expense.id}
-                  expense={expense}
-                  handleUpdate={this.props.handleUpdateExpense}
-                  handleDestory={this.props.handleDestroyExpense}
-                  updateBudget={this.handleBudgetChange}
-                />
-              </div>
-            )
-          }
-        </div>
       </React.Fragment>
     );
   }
 }
-
-const mapStateToProps = state => ({
-  expenses: state.expenses,
-});
-
-const mapDispatchToProps = (dispatch, getState) => ({
-  handleCreateExpense: expense => dispatch(expenseCreate(expense)),
-  handleUpdateExpense: expense => dispatch(expenseUpdate(expense)),
-  handleDestroyExpense: expense => dispatch(expenseDestroy(expense)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CategoryItem);
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import { categoryCreate, categoryUpdate, categoryDestroy } from '../../app/actions/categories.js';
+import { expenseCreate, expenseDestroy, expenseUpdate } from '../../app/actions/expenses.js';
+
 import CategoryForm from '../category-form';
 import CategoryItem from '../category-item/index.js';
+import ExpenseForm from '../expense-form/index.js';
+import ExpenseItem from '../expense-item/index.js';
 
-import {connect} from 'react-redux';
-import { categoryCreate, categoryUpdate, categoryDestroy} from '../../app/actions/categories.js';
 
 class Dashboard extends React.Component {
 
@@ -19,18 +24,48 @@ class Dashboard extends React.Component {
             <h2>Add a New Budget Category</h2>
             <CategoryForm handler={this.props.handleCreateCategory}/>
           </div>
-          <div className="expense-categories">
-            {
-              this.props.categories.map((category, i) => 
-                <div key={i} className="category">
-                  <CategoryItem 
-                    key={i} 
-                    category={category}
-                    handleUpdate={this.props.handleUpdateCategory}
-                    handleDestroy={this.props.handleDestroyCategory}
-                  /> 
-                </div>)
-            }
+          <div className="budget-content">
+            <div className="expense-categories">
+              {
+                this.props.categories.map((category, i) => 
+                  <div key={i} className="category">
+                    <CategoryItem 
+                      key={i} 
+                      category={category}
+                      expenses={this.props.expenses}
+                      handleUpdate={this.props.handleUpdateCategory}
+                      handleDestroy={this.props.handleDestroyCategory}
+                    /> 
+                  </div>)
+              }
+            </div>
+            <div className="expense-items">
+              {
+                this.props.categories.map((category, i) =>
+                  <div key={i * 1.45923}>
+                    <div className="expense-form">
+                      <h4>Add An Expense Item: </h4>
+                      <ExpenseForm handler={this.props.handleCreateExpense} category={category} />
+                    </div>
+                    {
+                      this.props.expenses[category.id].map((expense, i) =>
+                        <div key={i * 1.25873}>
+                          <div key={i} className="expense-item">
+                            <ExpenseItem
+                              key={expense.id}
+                              expense={expense}
+                              handleUpdate={this.props.handleUpdateExpense}
+                              handleDestory={this.props.handleDestroyExpense}
+                              updateBudget={this.handleBudgetChange}
+                            />
+                          </div>
+                        </div>)
+                    }
+                  </div>
+                )
+              
+              }
+            </div>
           </div>
         </section>
       </React.Fragment>
@@ -41,12 +76,16 @@ class Dashboard extends React.Component {
 
 const mapStateToProps = state => ({
   categories: state.categories,
+  expenses: state.expenses,
 });
 
 const mapDispatchToProps = (dispatch, getState) => ({
   handleCreateCategory: category => dispatch(categoryCreate(category)),
   handleUpdateCategory: category => dispatch(categoryUpdate(category)),
   handleDestroyCategory: category => dispatch(categoryDestroy(category)),
+  handleCreateExpense: expense => dispatch(expenseCreate(expense)),
+  handleUpdateExpense: expense => dispatch(expenseUpdate(expense)),
+  handleDestroyExpense: expense => dispatch(expenseDestroy(expense)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
